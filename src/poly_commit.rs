48eb483a009f441680c915a6bd6587ca02917b10
Almonds-alt <june@graphicPC.localdomain>
2025-08-07 22:50:51 +1000

    trait changes

--- src/poly_commit.rs
+++ src/poly_commit.rs
@@ -1,11 +1,13 @@
 use ark_ec::pairing::Pairing;
 use ark_poly::univariate::DensePolynomial;
 
-pub trait PolyCommit<E: Pairing, PK, SK, Commitment, Proof> {
+pub trait PolyCommit<E: Pairing, PK, SK, Commitment, Evaluation, Proof> {
     fn new() -> Self;
     fn setup(&mut self, max_deg: i32) -> (PK, SK); // throw away SK
-    fn commit(&self, pk: &PK, poly: &DensePolynomial<E::ScalarField>) -> Commitment;
+    fn commit(&self, pk: &PK, poly: &[DensePolynomial<E::ScalarField>]) -> Vec<Commitment>;
+    // Evaluation represents the evaluation of one polynomial at a bunch of points.
+    fn evaluate(&self, poly: &[DensePolynomial<E::ScalarField>], z: &[E::ScalarField]) -> Vec<Evaluation>;
     // first poly first z is first v, first poly second z is second v, ... second poly first z is (t+1)th v
-    fn open(&self, pk: &PK, poly: &[DensePolynomial<E::ScalarField>], z: &[E::ScalarField], v: &[E::ScalarField]) -> Proof;
-    fn verify(&self, c: &[Commitment], pk: &PK, p: &Proof, z: &[E::ScalarField], v: &[E::ScalarField]) -> bool;
+    fn open(&self, pk: &PK, poly: &[DensePolynomial<E::ScalarField>], z: &[E::ScalarField], v: &[Evaluation]) -> Proof;
+    fn verify(&self, c: &[Commitment], pk: &PK, p: &Proof, z: &[E::ScalarField], v: &[Evaluation]) -> bool;
 }
